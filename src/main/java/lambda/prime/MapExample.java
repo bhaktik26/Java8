d74a3fe21edcb0bfa93e7e5877a8b2978dd6ddf7
Bhakti <bhakti.kapadia@bhge.com>
2018-02-07 12:37:43 +0530

    CHANGES IN MAPEXAMPLE

--- src/main/java/lambda/prime/MapExample.java
+++ src/main/java/lambda/prime/MapExample.java
@@ -7,7 +7,7 @@
  * with the terms and conditions stipulated in the agreement/contract
  * under which the software has been supplied.
  */
- 
+
 package lambda.prime;
 
 import java.util.HashMap;
@@ -25,20 +25,20 @@ public class MapExample {
 	public static void main(String[] args) {
 		Map<Integer, String> map = new HashMap<>();
 		map.put(0, "a");
-		for(int i = 0 ; i<5; i++)
+		// putIfAbsent puts the value for the key if its absent
+		for (int i = 0; i < 5; i++)
 			map.putIfAbsent(i, "value" + i);
-		System.out.println(map.entrySet());
-		map.computeIfAbsent(5, num -> "b");
-		System.out.println(map.get(5));
-		System.out.println(map.getOrDefault(6, "Key doesnt exist"));
-		System.out.println(map.remove(5, "c"));
-		System.out.println(map.remove(5, "b"));
-		System.out.println(map.get(5));
-		map.merge(7, "val7", (val,newVal) -> val.concat(newVal));
-		System.out.println(map.get(7));
-		map.merge(7, "7", (val,newVal) -> val.concat(newVal));
-		System.out.println(map.get(7));
-		
+		System.out.println("The map is: " + map.entrySet());
+		map.computeIfAbsent(5, num -> num + "b");
+		System.out.println("Value corresponding to key=5: " + map.get(5));
+		System.out.println("If value for the key isnt present: " + map.getOrDefault(6, "Key doesnt exist"));
+		System.out.println("Removes if value matches: " + map.remove(5, "c"));
+		System.out.println("Removed: " + map.remove(5, "5b"));
+		System.out.println("Value is: " + map.get(5));
+		map.merge(7, "val7", (val, newVal) -> val.concat(newVal));
+		System.out.println("Value assigned: " + map.get(7));
+		map.merge(7, "7", (val, newVal) -> val.concat(newVal));
+		System.out.println("Value merged: " + map.get(7));
 
 	}
 
